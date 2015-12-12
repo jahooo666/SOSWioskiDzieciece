ac07719f6b737a115c1a3e27aaa28785a455cbdd
Marcin <dlugopix@gmail.com>
2015-12-12 18:33:00 +0100

    poprawione otwieranie zdjęia

--- OverlayingImages.py
+++ OverlayingImages.py
@@ -1,15 +1,15 @@
 from PIL import Image
 
 #forgrBackgrRatio = 1/3 #stosunek wielkosci obrazka nakladanego do tla na ktore nakladamy
+def overlay(photo):
+    background = photo #w maksymalnej rozdzielczosci jaka sie uda pobrac
+    foreground = Image.open("percent.png")
 
-background = Image.open("profilowe.jpg") #w maksymalnej rozdzielczosci jaka sie uda pobrac
-foreground = Image.open("percent.png")
+    bsize = background.size[0] #szerokosc ale i wysokosc bo bedziemy mieli kwadrat wiec luzik
+    fsize = foreground.size[0]
 
-bsize = background.size[0] #szerokosc ale i wysokosc bo bedziemy mieli kwadrat wiec luzik
-fsize = foreground.size[0]
+    newFsize = bsize/3 #forgrBackgrRatio
+    foreground = foreground.resize((newFsize,newFsize), Image.ANTIALIAS)#procent zresizowany do rozmiarow
 
-newFsize = bsize/3 #forgrBackgrRatio
-foreground = foreground.resize((newFsize,newFsize), Image.ANTIALIAS)#procent zresizowany do rozmiarow
-
-background.paste(foreground, (bsize-newFsize, bsize-newFsize), foreground)
-background.show()
+    background.paste(foreground, (bsize-newFsize, bsize-newFsize), foreground)
+    return background
--- SOSWioskiDzieciece.py
+++ SOSWioskiDzieciece.py
@@ -1,32 +1,27 @@
+import cStringIO
+import urllib
+
 from flask import Flask, request, render_template
 from PIL import Image
 import requests
-import os
-
+import OverlayingImages
 
 app = Flask(__name__)
-token = 'CAACEdEose0cBAADz6rHdZBvyZBvJGZAWHmqXCMaFB3ZCI1ZAk2VUtetOV90S7UKXipdoK7Mi74rH9RZBfwZCkZB6JwdDdmZB1ZA1nz54ZABQlCeClkoX49Ll8ZAmXfnBoW8HnGHJZCXl3qZCQYmTrNt4tGWa3CTZCVUDRC31qQn16Tm4nElKpWAbJ2TI1KF7AWuG99gYhNRupZCTDQgOOfWgjr0ZCQmds'
+app.debug = True
+token = ''
+
 
 @app.route('/')
 def hello_world():
-    qs = 'https://graph.facebook.com/me/picture?type=square&height=3000&width=3000' + '&access_token='+token
-    res = requests.get(qs)
-    #print res.text
-    photo_s = res.content
-    photo = open('/Users/Marcin/PycharmProjects/SOSWioskiDzieciece/photo.jpg', 'w')
-    photo.write(photo_s)
-    photo.close()
+    qs = 'https://graph.facebook.com/me/picture?type=square&height=3000&width=3000' + '&access_token=' + token
+    res = requests.request('GET', qs)
+    file = cStringIO.StringIO(res.content)
+    img = Image.open(file)
+    img = OverlayingImages.overlay(img)
+    img.show()
 
-    # photo = open('/Users/Marcin/PycharmProjects/SOSWioskiDzieciece', 'w')
-    #print(photo)
-    #photo.write(r.content)
-    #photo.close()
-    #print qs
-    #print r.json()
-    #image = Image.open(photo)
-    #image.show()
-    return render_template('templates.html')
 
+    return render_template('templates.html', img = img )
 
 if __name__ == '__main__':
     app.run()
