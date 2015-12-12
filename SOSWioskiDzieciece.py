5a87eac89e18f9a1e7ed331dd20997ac708bf4d9
jahooo <okraskaj@ee.pw.edu.pl>
2015-12-12 16:58:29 +0100

    pobieranie z facebooka

--- SOSWioskiDzieciece.py
+++ SOSWioskiDzieciece.py
@@ -1,13 +1,27 @@
 from flask import Flask, request
 from PIL import Image
+import requests
+import os
+
 
 app = Flask(__name__)
-
+token = 'CAACEdEose0cBAADz6rHdZBvyZBvJGZAWHmqXCMaFB3ZCI1ZAk2VUtetOV90S7UKXipdoK7Mi74rH9RZBfwZCkZB6JwdDdmZB1ZA1nz54ZABQlCeClkoX49Ll8ZAmXfnBoW8HnGHJZCXl3qZCQYmTrNt4tGWa3CTZCVUDRC31qQn16Tm4nElKpWAbJ2TI1KF7AWuG99gYhNRupZCTDQgOOfWgjr0ZCQmds'
 
 @app.route('/')
 def hello_world():
-
-    return 'Hello World!'
+    qs = 'https://graph.facebook.com/me/picture?type=square&height=3000&width=3000' + '&access_token='+token
+    res = requests.get(qs)
+    print res.text
+    res.show()
+    # photo = open('/photo.jpg', 'w')
+    #print(photo)
+    #photo.write(r.content)
+    #photo.close()
+    #print qs
+    #print r.json()
+    #image = Image.open(photo)
+    #image.show()
+    return  'hello'
 
 
 if __name__ == '__main__':
