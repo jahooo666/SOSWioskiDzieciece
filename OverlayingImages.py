91400d4589f76c35c99848c91e7100082dda6fad
jahooo <okraskaj@ee.pw.edu.pl>
2015-12-12 14:22:20 +0100

    Procent wyskalowany i przesuniety do prawego dolnego rogu.

--- OverlayingImages.py
+++ OverlayingImages.py
@@ -1,7 +1,15 @@
 from PIL import Image
 
-background = Image.open("profilowe.jpg")
+forgrBackgrRatio = 1/3 #stosunek wielkosci obrazka nakladanego do tla na ktore nakladamy
+
+background = Image.open("profilowe.jpg") #w maksymalnej rozdzielczosci jaka sie uda pobrac
 foreground = Image.open("percent.png")
 
-background.paste(foreground, (0, 0), foreground)
+bsize = background.size[0] #szerokosc ale i wysokosc bo bedziemy mieli kwadrat wiec luzik
+fsize = foreground.size[0]
+
+newFsize = bsize * forgrBackgrRatio
+foreground = foreground.resize((newFsize,newFsize), Image.ANTIALIAS)#procent zresizowany do rozmiarow
+
+background.paste(foreground, (bsize-newFsize, bsize-newFsize), foreground)
 background.show()
