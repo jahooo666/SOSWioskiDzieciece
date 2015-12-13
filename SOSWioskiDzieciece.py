318034d40306c0ed7564497c7b2f6a7cf9e6ab22
jahooo666 <okraskaj@ee.pw.edu.pl>
2015-12-13 02:29:10 +0100

    W komentarzach jest jedna dzialajaca wersja z dzialajacymna stale plikiem, a ogolnie to wwszystk jest rozgrzebane i nie dziala.

--- SOSWioskiDzieciece.py
+++ SOSWioskiDzieciece.py
@@ -12,12 +12,12 @@ import OverlayingImages
 
 app = Flask(__name__)
 app.debug = True
-token = "CAACIe34ksn8BAO3pPCorbzHlbfMSYTYJL0p1ieCY0tO6OCKZB3UpudXxX0sGZBlZAwQBai5SClEVY1H3g7faQl3PbNU5sI1cj6ny4FGVp7hCYB1GAKX2xkAaV580UzsSZApEvgwzn9sq746CaOGvZCVoZAGpQHaklxrfAM6RwO2dSeJnyxPfGcDXoBv0pmWKulHZAxQf48H9wZDZD"
-
+token = "CAACEdEose0cBADVf0pe9CLFZAgXRZABGglRFmFgXb8u4u73xjnoqQNkTp6XYhCsZAn3tHbKc6mLxTS5jKboNfSmsFFJD7jGDJT5PVaeWZCfTQx673MCtufgspSbZCaXeKPGZBGsBP4jyOYYBQCpWC9ZBc1429272zpz6ldHRqnD6TFEd6H1GaZCtBqVoTJ6ogQw4ibp4gpnywQZDZD"
+caly = '&access_token=' + token
 
 @app.route('/', methods = ['GET', 'POST'])
 def hello_world():
-    qs = 'https://graph.facebook.com/me/picture?type=square&height=3000&width=3000' + '&access_token=CAACIe34ksn8BAO3pPCorbzHlbfMSYTYJL0p1ieCY0tO6OCKZB3UpudXxX0sGZBlZAwQBai5SClEVY1H3g7faQl3PbNU5sI1cj6ny4FGVp7hCYB1GAKX2xkAaV580UzsSZApEvgwzn9sq746CaOGvZCVoZAGpQHaklxrfAM6RwO2dSeJnyxPfGcDXoBv0pmWKulHZAxQf48H9wZDZD'
+    qs = 'https://graph.facebook.com/me/picture?type=square&height=3000&width=3000' + caly
     res = requests.request('GET', qs)
     file = cStringIO.StringIO(res.content)
     img = Image.open(file)
@@ -36,7 +36,24 @@ def hello_world():
 @app.route('/token', methods=['GET', 'POST'])
 def token():
     if request.method == 'POST':
+        print 'post'
         if request.form.get('submit')=='Wstaw zdjecie':
+            #ponizej podejscie z tokenem i nawet adresem ustawionym na sztywno DZiala!
+            #req = "https://graph.facebook.com/me/photos/?url=http://hodowanko.pl/application/assets/styles/common/images/piesel.png"+'&access_token=CAACEdEose0cBADVf0pe9CLFZAgXRZABGglRFmFgXb8u4u73xjnoqQNkTp6XYhCsZAn3tHbKc6mLxTS5jKboNfSmsFFJD7jGDJT5PVaeWZCfTQx673MCtufgspSbZCaXeKPGZBGsBP4jyOYYBQCpWC9ZBc1429272zpz6ldHRqnD6TFEd6H1GaZCtBqVoTJ6ogQw4ibp4gpnywQZDZD'
+
+            req = "https://graph.facebook.com/me/photos/"+'&access_token=CAACEdEose0cBADVf0pe9CLFZAgXRZABGglRFmFgXb8u4u73xjnoqQNkTp6XYhCsZAn3tHbKc6mLxTS5jKboNfSmsFFJD7jGDJT5PVaeWZCfTQx673MCtufgspSbZCaXeKPGZBGsBP4jyOYYBQCpWC9ZBc1429272zpz6ldHRqnD6TFEd6H1GaZCtBqVoTJ6ogQw4ibp4gpnywQZDZD'
+
+            url = "https://graph.facebook.com/me/photos/"
+            header={'Authorization': 'access_token CAACEdEose0cBADVf0pe9CLFZAgXRZABGglRFmFgXb8u4u73xjnoqQNkTp6XYhCsZAn3tHbKc6mLxTS5jKboNfSmsFFJD7jGDJT5PVaeWZCfTQx673MCtufgspSbZCaXeKPGZBGsBP4jyOYYBQCpWC9ZBc1429272zpz6ldHRqnD6TFEd6H1GaZCtBqVoTJ6ogQw4ibp4gpnywQZDZD' }
+            files={'profilowe.jpg': open('profilowe.jpg', 'rb')}
+
+            r = requests.post(url,header,files)
+
+            #r = requests.post(req, files={'profilowe.jpg': open('profilowe.jpg', 'rb')})
+            #tak jak w linijce wyzej powinno sie wstawiac to jest gotowy request ale nie wiem czemu wywala ze nie ma tokena-probowalem na rozne sposoby i wszystkie sie jebia
+            #r = requests.post(req)
+            print r.content
+            print r
             print "zmieniamy zdjecie"
         if request.form.get('submit')=='Dodaj post':
             print "dodajemy post"
