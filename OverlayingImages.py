0f6c4c572ad069d2dd55d1d139506569ab869fe3
jahooo <okraskaj@ee.pw.edu.pl>
2015-12-13 15:32:45 +0100

    Dodaje obrazek wiosek dzieciecych- nie wiem tlko czy w dobrym miejscu

--- OverlayingImages.py
+++ OverlayingImages.py
@@ -4,12 +4,15 @@ from PIL import Image
 def overlay(photo):
     background = photo #w maksymalnej rozdzielczosci jaka sie uda pobrac
     foreground = Image.open("percent.png")
+    wioski = Image.open("wioski.png")
 
     bsize = background.size[0] #szerokosc ale i wysokosc bo bedziemy mieli kwadrat wiec luzik
     fsize = foreground.size[0]
 
     newFsize = bsize/3 #forgrBackgrRatio
     foreground = foreground.resize((newFsize,newFsize), Image.ANTIALIAS)#procent zresizowany do rozmiarow
+    wioski = wioski.resize((newFsize,newFsize), Image.ANTIALIAS)#wioski zresizowane do rozmiarow
 
     background.paste(foreground, (bsize-newFsize, bsize-newFsize), foreground)
+    background.paste(wioski, (bsize-2*newFsize, bsize-newFsize),wioski)
     return background
